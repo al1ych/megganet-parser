347f6b0c76f8f7cd082b5523770e295807bbc322
al1ych <decter49pro@gmail.com>
2024-12-09 01:18:59 +0300

    ч

--- functions/parser.js
+++ functions/parser.js
@@ -1,12 +1,20 @@
+// @hourly
 // Netlify Scheduled Function
 
 const { parse } = require("../mainParser")
 
-// @hourly
 exports.handler = async () => {
   try {
-    parse()
+    await parse() // Вызов функции вашего парсера
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: "Парсер успешно выполнен!" }),
+    }
   } catch (e) {
-    console.log("Error starting parser...")
+    console.error("Error starting parser:", e)
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "Ошибка выполнения парсера" }),
+    }
   }
 }
